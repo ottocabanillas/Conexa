092bb6fe159e295f5e82e20d33d8c11044c7f405
Oscar A. Rafael Cabanillas <ottocabanillas@MacBook-Pro-de-Oscar.local>
2024-07-09 22:54:44 -0300

    [Changed] New Project

---
+++ Conexa-Challenge/Maps/Model/UserData.swift
@@ -0,0 +1,8 @@
+//
+//  UserData.swift
+//  Conexa-Challenge
+//
+//  Created by Oscar A. Rafael Cabanillas on 09/07/2024.
+//
+
+import Foundation
---
+++ Conexa-Challenge/News/Model/NewsData.swift
@@ -0,0 +1,8 @@
+//
+//  NewsData.swift
+//  Conexa-Challenge
+//
+//  Created by Oscar A. Rafael Cabanillas on 09/07/2024.
+//
+
+import Foundation
